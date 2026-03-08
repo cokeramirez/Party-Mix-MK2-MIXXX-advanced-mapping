f6843ad7261ce40c976b41729a2d6546ca188491
cokeramirez <cokeramirez@gmail.com>
2026-03-08 16:39:41 -0300

    updated inertia variables
    10ms was less than the minimum 20ms timer of mixxx
    100ms was too big

--- Numark-Party-Mix-MK2-newjog.js
+++ Numark-Party-Mix-MK2-newjog.js
@@ -42,8 +42,8 @@ var NumarkPartyMix = function() {
     var isScratchEnabled = { 1: true, 2: true }; 
 
     // --- VARIABLES DE CONFIGURACIÓN ---
-    var INERTIA_TIMEOUT_MS = 10;        
-    var INERTIA_TIMEOUT_MS_PAUSE = 100; 
+    var INERTIA_TIMEOUT_MS = 20;        
+    var INERTIA_TIMEOUT_MS_PAUSE = 50; 
     var POST_SCRATCH_LOCKOUT_MS = 250; 
     var PAUSE_JOG_SENSITIVITY = 0.1;    
 
